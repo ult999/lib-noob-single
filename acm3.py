bf7f86867264d6d61bbb7a5a9314dfe49dae2e1d
ult999 <42420708+ult999@users.noreply.github.com>
2018-08-23 20:18:24 +0530

    Add files via upload

---
+++ acm3.py
@@ -0,0 +1,25 @@
+fp = open('PILANI-PR-Numbers-SEM1-2018-2019.pdf', 'rb')
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
+
+parser = PDFParser(fp)
+doc = PDFDocument()
+parser.set_document(doc)
+doc.set_parser(parser)
+doc.initialize('')
+rsrcmgr = PDFResourceManager()
+laparams = LAParams()
+laparams.char_margin = 1.0
+laparams.word_margin = 1.0
+device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+interpreter = PDFPageInterpreter(rsrcmgr, device)
+extracted_text = ''
+
+for page in doc.get_pages():
+    interpreter.process_page(page)
+    layout = device.get_result()
+    for lt_obj in layout:
+        if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+            extracted_text += lt_obj.get_text()
